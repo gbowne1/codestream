a1722d047900b9ed84780d02475be7eb57258df6
Gregory Bowne <gbowne144@gmail.com>
2026-02-21 18:15:05 -0800

    Delete src/paths.js
    Oops wrong place

--- src/paths.js
+++
@@ -1,9 +0,0 @@
-import { fileURLToPath } from 'url';
-import { dirname, join } from 'path';
-
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
-
-// Point to the root of the project or the data folder
-export const rootDir = join(__dirname, '../../');
-export const streamsFilePath = join(rootDir, 'streams.json');
